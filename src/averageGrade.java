f1e039df3fa95c7c1ae0b241f84f411df20684c2
Okiki-omisande <65607886+Okiki-omisande@users.noreply.github.com>
2022-03-28 19:30:15 +0100

    Refactor averageGrade src

--- src/averageGrade.java
+++ src/averageGrade.java
@@ -2,11 +2,10 @@ import java.util.Scanner;
 
 public class averageGrade {
     public static void main(String[] args) {
-        int StudentGrade = 0;
         int GradeCounter = 1;
         int Total = 0;
 
-        while (StudentGrade <= 100 && GradeCounter <= 10) {
+        while (GradeCounter <= 10) {
             System.out.print("Enter student grade: ");
             Scanner input = new Scanner(System.in);
             int Grade = input.nextInt();
